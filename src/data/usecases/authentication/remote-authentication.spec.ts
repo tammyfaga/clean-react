f3d4981cd655824ba07179721e933dda76847c64
Tammy Fagá <tammyfaga@gmail.com>
2021-06-23 20:50:31 -0300

    style: eslint

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,24 +1,24 @@
-import { RemoteAuthentication } from "./remote-authentication";
-import { HttpPostClientSpy } from "../../test/mock-http-client";
+import { RemoteAuthentication } from './remote-authentication'
+import { HttpPostClientSpy } from '../../test/mock-http-client'
 
 type SutTypes = {
-  sut: RemoteAuthentication;
-  httpPostClientSpy: HttpPostClientSpy;
+  sut: RemoteAuthentication
+  httpPostClientSpy: HttpPostClientSpy
 };
 
-const makeSut = (url: string = "any_url"): SutTypes => {
-  const httpPostClientSpy = new HttpPostClientSpy();
-  const sut = new RemoteAuthentication(url, httpPostClientSpy);
+const makeSut = (url: string = 'any_url'): SutTypes => {
+  const httpPostClientSpy = new HttpPostClientSpy()
+  const sut = new RemoteAuthentication(url, httpPostClientSpy)
   return {
     sut,
-    httpPostClientSpy,
-  };
-};
-describe("RemoteAuthentication", () => {
-  const url = "other_url";
-  const { sut, httpPostClientSpy } = makeSut(url);
-  test("Should call HttpPostClient with correct URL", async () => {
-    await sut.auth();
-    expect(httpPostClientSpy.url).toBe(url);
-  });
-});
+    httpPostClientSpy
+  }
+}
+describe('RemoteAuthentication', () => {
+  const url = 'other_url'
+  const { sut, httpPostClientSpy } = makeSut(url)
+  test('Should call HttpPostClient with correct URL', async () => {
+    await sut.auth()
+    expect(httpPostClientSpy.url).toBe(url)
+  })
+})
