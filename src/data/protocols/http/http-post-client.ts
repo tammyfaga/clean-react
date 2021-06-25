d725d23009eebcbcaf7a074135f5c008e3b3cfa9
Tammy Fagá <tammyfaga@gmail.com>
2021-06-24 21:48:34 -0300

    chore: off camelCase eslint

--- src/data/protocols/http/http-post-client.ts
+++ src/data/protocols/http/http-post-client.ts
@@ -1,7 +1,7 @@
 export type HttpPostParams = {
   url: string
   body?: object
-}
+};
 export interface HttpPostClient {
-  post (params: HttpPostParams): Promise<void>
+  post(params: HttpPostParams): Promise<void>
 }
